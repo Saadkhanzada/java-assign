0857ebcfdddf9db38ba220f417aaa70b9a9c5b69
Saad Khan <100066385+Saadkhanzada@users.noreply.github.com>
2022-04-13 19:33:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+
+
+// -----------string methods------------
+
+// --------question 1
+
+// var firstName = prompt("Enter your first name")
+// var lastName = prompt("Enter your last name")
+// var fullName = firstName +" " + lastName
+// alert(fullName) 
+
+
+// ----------question 2
+
+
+// var userInput = prompt("enter phone name")
+
+// var mobilePhones = ["metrolla" , "iphone x" , "iphone 13 pro max" , "samsung galaxy s6 edge"]
+
+// document.write("My favourite phone is: " + mobilePhones[3]+ "<br />")
+// document.write("Length of string: " + mobilePhones.indexOf("samsung galaxy s6 edge"))
+
+
+
+// -------question 3
+
+// var pak = "Pakistani"
+
+// document.write("string: "+ pak + "<br />" + "Index of 'n' is: " + pak.indexOf("n"))
+
+
+// ----------question 4
+
+
+// var letter = "Hello World"
+
+// document.write("string: " + letter + "<br />" + "Lst index of 'l': " + letter.lastIndexOf("l"))
+
+
+// -----------question 5
+
+// var letter = "Pakistani"
+
+// document.write("string: " + letter + "<br />" + "character at index 3: " + letter.charAt(3))
+
+
+// ---------------question 6
+
+// var firstName = prompt("Enter your first name")
+// var lastName = prompt("Enter your last name")
+// var fullName = firstName.concat(lastName)
+// alert(fullName)
+
+
+// ------------question 10
+
+// var letter = prompt("enter small letter")
+
+// document.write("Userinput : " + letter + "<br />" + "Upper case: " + letter.toUpperCase())
+
+
+// ---------question 11
+
+// var letter = prompt("enter small letter")
+
+// document.write("Userinput : " + letter + "<br />" + "Tittle case : " + letter.)
+
+
+// -------------question 18
+
+// var letter = "The quick brown fox jumps over the lazy dog"
+
+// document.write("Text: " + letter)
+// document.write()
+
+
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+let word = myArray[0] + myArray[1] 
+
+document.write(word)
